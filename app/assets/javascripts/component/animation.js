a376bc6014478041886ff54ae52ebb3c53dcd7f2
Kathryn Ragudos <kragudos19@gmail.com>
2016-11-26 13:50:23 -0800

    fixes the right column in services section

--- app/assets/javascripts/component/animation.js
+++ app/assets/javascripts/component/animation.js
@@ -34,10 +34,20 @@ $(function(){
         /*---------------------------------------*/
         /*  CONTENT BOXES
         /*---------------------------------------*/
-        $('.content-list-text').css('opacity', 0).one('inview', function(isInView){
-            if (isInView) {$(this).addClass('animated fadeInDown delayp6').css('opacity', 1);}
+        // $('.content-list-text').css('opacity', 0).one('inview', function(isInView){
+        //     if (isInView) {$(this).addClass('animated fadeInDown delayp5').css('opacity', 1);}
+        // });
+        $('#meet-the-team').find('.content-list-text').each(function(i){            
+            var element = $(this),
+            itemsDelay   = ( isNaN($(this).data('animation-delay')) ? 50 : $(this).data('animation-delay'));
+            element.css('opacity', 0).one('inview', function(isInView) {
+                if (isInView){
+                    setTimeout(function(){
+                        element.addClass('animated fadeInDown').css('opacity', 1);
+                    } , itemsDelay * (i * 2));
+                }
+            });
         });
-        
         
         /*---------------------------------------*/
         /*  COUNTERS
